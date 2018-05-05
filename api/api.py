1d94e1fe223554fcab288241089d7320930ad40d
archibishop <wagubib@gmail.com>
2018-05-04 20:45:21 -0700

    ADDING UTILS FILE

--- api/api.py
+++ api/api.py
@@ -6,6 +6,10 @@ from models.admin import Admin
 from models.meals import Meals
 from models.order import Order
 
+from utils import is_loged_in
+from utils import is_user
+from utils import is_admin
+
 from flasgger import Swagger
 
 app = Flask(__name__)
@@ -20,43 +24,6 @@ order = Order()
 
 
 Swagger(app)
-
-""" Check if user is logged """
-
-def is_loged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            return jsonify({'message': "Unauthorized Access, Please Login"})
-    return wrap
-
-""" Check if user """
-
-
-def is_user(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'userV' in session:
-            return f(*args, **kwargs)
-        else:
-            return jsonify({'message': "Unauthorized Access,\
-             You are not an admin"})
-    return wrap
-
-""" Check if Admin """
-
-
-def is_admin(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'admin' in session:
-            return f(*args, **kwargs)
-        else:
-            return jsonify({'message':\
-             "Unauthorized Access, You are not an admin"})
-    return wrap
 
 
 """ Registering a User """
---
+++ api/utils.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from flask import session
+""" Check if user is logged """
+
+def is_loged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'message': "Unauthorized Access, Please Login"})
+    return wrap
+
+""" Check if user """
+
+
+def is_user(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'userV' in session:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'message': "Unauthorized Access,\
+             You are not an admin"})
+    return wrap
+
+""" Check if Admin """
+
+
+def is_admin(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'admin' in session:
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'message':\
+             "Unauthorized Access, You are not an admin"})
+    return wrap
