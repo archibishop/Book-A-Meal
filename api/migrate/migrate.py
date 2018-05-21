592b07dd2f21bbe7dc63a33c8edd973f7db74b17
archibishop <wagubib@gmail.com>
2018-05-21 06:26:00 -0700

    ADD DATABASE URI IN MIGRATIONS

--- api/migrate/migrate.py
+++ api/migrate/migrate.py
@@ -9,7 +9,7 @@ import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] =\
- "postgresql://postgres:12345@localhost/book"
+ "postgres://jbgpiruhjzgafa:3bcdc8753255e485e6e96311b7fb7f164c63fc1660faa518669855d92f60109e@ec2-50-19-224-165.compute-1.amazonaws.com:5432/ddk6pbihffrbaik"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db =  SQLAlchemy(app)
